147afb2bd412250613d8c3c21a26701701b89d6f
RAMANS <subha_raman_external@whirlpool.com>
2018-06-13 15:57:28 +0100

    Added src package

---
+++ src/com/sample/today/Today.java
@@ -0,0 +1,13 @@
+package com.sample.today;
+
+import java.util.Calendar;
+
+public class Today {
+	
+	public static void main(String args[] ){
+		
+		System.out.println(Calendar.getInstance().getTime().toString());
+		
+	}
+
+}
